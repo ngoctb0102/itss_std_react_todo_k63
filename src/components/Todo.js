8de436d3cb1298a62f194cde215b14d56ae87361
ngoctb0102 <nam09368@gmail.com>
2022-06-08 03:46:38 +0000

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,21 @@
 import React from 'react';
-
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
-/* コンポーネント */
-import TodoItem from './TodoItem';
-import Input from './Input';
-import Filter from './Filter';
-
 /* カスタムフック */
 import useStorage from '../hooks/storage';
+import Filter from './Filter';
+import Input from './Input';
+/*
+  【Todoのデータ構成】
+ ・key：Todoを特定するID（String）
+ ・text：Todoの内容（String）
+ ・done：完了状態（Boolean true:完了済み,, false:未完了）
+*/
+/* コンポーネント */
+import TodoItem from './TodoItem';
 
-/* ライブラリ */
-import {getKey} from "../lib/util";
+const filterItems = ['全て', '未完了', '完了済み'];
 
 function Todo() {
-  const [items, setItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
   const [filter, setFilter] = React.useState('全て');
 
   const filterData = React.useMemo(() => {
@@ -37,19 +27,33 @@ function Todo() {
       return items.filter((item) => item.done);
     }
   }, [filter, items]);
-const addTodo = (todo) => setItems([...items, todo]);
+
+  const handleClick = (key) =>
+    putItems(
+      items.map((item) => {
+        if (item.key === key) {
+          return { ...item, done: !item.done };
+        }
+        return item;
+      })
+    );
+
+  const addTodo = (todo) => putItems([...items, todo]);
 
   return (
     <div className="panel">
-      <div className="panel-heading">
-        ITSS ToDoアプリ
-      </div>
+      <div className="panel-heading">ITSS ToDoアプリ</div>
       <Input addTodo={addTodo} />
       <Filter filterItems={filterItems} filter={filter} setFilter={setFilter} />
       {filterData.map((item) => (
         <TodoItem handleClick={handleClick} key={item.key} item={item} />
       ))}
       <div className="panel-block">{filterData.length} items</div>
+      <div className="panel-block">
+        <button onClick={clearItems} className="button is-fullwidth">
+          全てのToDoを削除する
+        </button>
+      </div>
     </div>
   );
 }
--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -12,19 +12,19 @@ import { useState, useEffect } from 'react';
 const STORAGE_KEY = 'itss-todo';
 
 function useStorage() {
-  const [items, setItems] = useState([]);
-　
-　/* 副作用を使う */
+  const [items, setItems] = useState([]); /* 副作用を使う */
   useEffect(() => {
-    
+    setItems(JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'));
   }, []);
 
-  const putItems = items => {
-    
+  const putItems = (items) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+    setItems(items);
   };
 
   const clearItems = () => {
-    
+    localStorage.clear();
+    setItems([]);
   };
 
   return [items, putItems, clearItems];
